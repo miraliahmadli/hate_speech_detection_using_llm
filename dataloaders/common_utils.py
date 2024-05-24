4e378eefac48bfc01490094c28ec3d67fd9bc759
Mirali Ahmadli <miraliahmadli@gmail.com>
2024-05-24 16:27:52 +0200

    Add CAD and gender hate speech dataset

--- dataloaders/common_utils.py
+++ dataloaders/common_utils.py
@@ -58,9 +58,10 @@ def load_and_process_toxigen():
         return labeled_annotations
 
     TG = load_dataset("skg/toxigen-data", name="train")
-    tg_dataset = pd.DataFrame(TG["train"])
+    tg_dataset = TG["train"].to_pandas()
+    tg_dataset = tg_dataset[["prompt_label", "generation"]]
     tg_dataset.rename(columns={"prompt_label": "label", "generation":"text"}, inplace=True)
-    tg_train_data = Dataset.from_pandas(tg_dataset).map(remove_columns=['prompt', 'generation_method', 'group', 'roberta_prediction'])
+    tg_train_data = Dataset.from_pandas(tg_dataset)
 
     tg_test_dateset = load_dataset("skg/toxigen-data", name="annotated")
     tg_human_eval = pd.DataFrame(tg_test_dateset["test"])
@@ -70,26 +71,61 @@ def load_and_process_toxigen():
     return tg_train_data, tg_test_data
 
 
-def load_and_process_twitter_data():
+def load_and_process_twitter_data(test_size=0.2):
     dataset = load_dataset('tweets_hate_speech_detection', name="default")
     trainval_data = dataset['train'].to_pandas()
     trainval_data.rename(columns={'tweet': 'text'}, inplace=True)
     trainval_data['label'] = trainval_data['label'].astype(int)
-    train_data, val_data = train_test_split(trainval_data, test_size=0.2)
+    train_data, val_data = train_test_split(trainval_data, test_size=test_size)
     train_data = Dataset.from_pandas(train_data)
     val_data = Dataset.from_pandas(val_data)
 
     return train_data, val_data
 
 
-def load_and_process_berkeley_data():
+def load_and_process_berkeley_data(test_size=0.2):
     dataset = load_dataset("ucberkeley-dlab/measuring-hate-speech")
     trainval_data = dataset['train'].to_pandas()
     trainval_data = trainval_data[["text", "hate_speech_score"]]
     trainval_data.rename(columns={'hate_speech_score': 'label'}, inplace=True)
     trainval_data['label'] = (trainval_data['label'] > 0.5).astype(int)
 
-    train_data, val_data = train_test_split(trainval_data, test_size=0.2)
+    train_data, val_data = train_test_split(trainval_data, test_size=test_size)
     train_data = Dataset.from_pandas(train_data)
     val_data = Dataset.from_pandas(val_data)
+    return train_data, val_data
+
+def load_and_process_gender_hate_speech_data():
+    dataset = load_dataset("ctoraman/gender-hate-speech")
+    train = dataset['train'].to_pandas()
+    train = train[["Text", "Label"]]
+    train.rename(columns={'Text': 'text', 'Label': 'label'}, inplace=True)
+    train['label'] = (train['label'] > 0.5).astype(int)
+
+    val = dataset['test'].to_pandas()
+    val = val[["Text", "Label"]]
+    val.rename(columns={'Text': 'text', 'Label': 'label'}, inplace=True)
+    val['label'] = (val['label'] > 0.5).astype(int)
+
+    train_data = Dataset.from_pandas(train)
+    val_data = Dataset.from_pandas(val)
+    return train_data, val_data
+
+def load_and_process_cad(dir_path='data/CAD'):
+    train_path = f"{dir_path}/train.csv"
+    dev_path = f"{dir_path}/dev.csv"
+    test_path = f"{dir_path}/test.csv"
+    train_data = pd.read_csv(train_path)
+    dev_data = pd.read_csv(dev_path)
+    train_data = pd.concat([train_data, dev_data])
+    test_data = pd.read_csv(test_path)
+    
+    train_data.drop(columns=['id'], inplace=True)
+    test_data.drop(columns=['id'], inplace=True)
+    
+    train_data.rename(columns={'labels': 'label'}, inplace=True)
+    test_data.rename(columns={'labels': 'label'}, inplace=True)
+
+    train_data = Dataset.from_pandas(train_data)
+    val_data = Dataset.from_pandas(test_data)
     return train_data, val_data
